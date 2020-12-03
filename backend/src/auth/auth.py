9de97abb003e875a39923bc9c1065aeb21414535
George Nady <gorgonady.gn@gmail.com>
2020-12-03 23:50:18 +0200

    2nd commit first review

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'omarelweshy1.auth0.com'
+AUTH0_DOMAIN = 'fsnd200.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee'
+API_AUDIENCE = 'coffee_shop'
 
 
 # AuthError Exception => DONE
